cdfff2b59cc468b532a784e852b506cbb82e616d
alikhan410 <mczeyo@gmail.com>
2025-07-23 19:15:11 +0500

    add ui from polariscomponents

---
+++ app/components/FeedbackCard.jsx
@@ -0,0 +1,32 @@
+import { Card, BlockStack, Text, Button, ButtonGroup, InlineStack } from '@shopify/polaris';
+import { XIcon, ThumbsDownIcon, ThumbsUpIcon } from "@shopify/polaris-icons";
+
+const FeedbackCard = () => {
+  return (
+    <Card>
+      <BlockStack gap='400' align='start'>
+        <BlockStack gap='200'>
+          <InlineStack align='space-between'>
+            <Text as='h2' variant='headingMd'>
+              Share your feedback
+            </Text>
+            <XIcon width='1rem' height='1rem' style={{ cursor: 'pointer' }} />
+          </InlineStack>
+          <Text as='p' variant='bodyMd' tone='subdued'>
+            How would you describe your experience using the Polaris Components app?
+          </Text>
+        </BlockStack>
+        <ButtonGroup>
+          <Button icon={ThumbsUpIcon} onClick={() => null}>
+            Good
+          </Button>
+          <Button icon={ThumbsDownIcon} onClick={() => null}>
+            Bad
+          </Button>
+        </ButtonGroup>
+      </BlockStack>
+    </Card>
+  );
+};
+
+export default FeedbackCard;
